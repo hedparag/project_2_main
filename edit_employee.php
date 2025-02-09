e9681f1c7614df4a7e1a177fc034eb13fabb2d53
Shivam Kumar Sah <sahfamily91@gmail.com>
2025-02-10 02:36:52 +0530

    head changed

--- edit_employee.php
+++ edit_employee.php
@@ -2,13 +2,8 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Management System - Edit Employee</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <?php include './templates/meta-info.php'; ?>
+    <title>Edit Employee | EMS</title>
 </head>
 
 <body class="d-flex flex-column min-vh-100" style="background-image: url('https://img.freepik.com/free-vector/blue-pink-halftone-background_53876-99004.jpg'); background-size: cover; background-position: center;"> <!-- Navbar -->
